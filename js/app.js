28eb2c0d0c7ba890f57677c6a6a091f6c3224ed5
Magdalena <magdalenaouirane@gmail.com>
2021-01-16 23:17:08 +0100

    add another way to add elements to html

--- js/app.js
+++ js/app.js
@@ -25,44 +25,51 @@ newDivEl.classList.add("item","my-3");
 
 newDivEl.appendChild(document.createTextNode(newItem));
 
-const h5 = document.createElement('h5');
-h5.classList.add("item-name","text-capitalize");
-newDivEl.appendChild(h5);
+newDivEl.innerHTML = `<h5 class="item-name text-capitalize">laundry</h5>
+<div class="item-icons">
+ <a href="#" class="complete-item mx-2 item-icon"><i class="far fa-check-circle"></i></a>
+ <a href="#" class="edit-item mx-2 item-icon"><i class="far fa-edit"></i></a>
+ <a href="#" class="delete-item item-icon"><i class="far fa-times-circle"></i></a>
+</div>`;
+
+// const h5 = document.createElement('h5');
+// h5.classList.add("item-name","text-capitalize");
+// newDivEl.appendChild(h5);
 
 
-const iconDiv = document.createElement('div');
-iconDiv.className = "item-icons";
-newDivEl.appendChild(iconDiv);
+// const iconDiv = document.createElement('div');
+// iconDiv.className = "item-icons";
+// newDivEl.appendChild(iconDiv);
 
-const link = document.createElement('a');
-link.setAttribute('href', "#");
-link.classList.add('complete-item', "mx2", "item-icon");
-iconDiv.appendChild(link);
+// const link = document.createElement('a');
+// link.setAttribute('href', "#");
+// link.classList.add('complete-item', "mx2", "item-icon");
+// iconDiv.appendChild(link);
 
-const circleCheck = document.createElement('i');
-circleCheck.classList.add("far","fa-check-circle");
-link.appendChild(circleCheck);
-
-
-
-link2 =document.createElement('a');
-link2.setAttribute('href', "#");
-link2.classList.add('edit-item', "mx2", "item-icon");
-iconDiv.appendChild(link2);
-
-const faEdit = document.createElement('i');
-faEdit.classList.add("far", "fa-edit");
-link2.appendChild(faEdit);
+// const circleCheck = document.createElement('i');
+// circleCheck.classList.add("far","fa-check-circle");
+// link.appendChild(circleCheck);
 
 
-link3 =document.createElement('a');
-link3.setAttribute('href', "#");
-link3.classList.add('delete-item', "item-icon");
-iconDiv.appendChild(link3);
 
-const circleTime = document.createElement('i');
-circleTime.classList.add("far", "fa-times-circle");
-link3.appendChild(circleTime);
+// link2 =document.createElement('a');
+// link2.setAttribute('href', "#");
+// link2.classList.add('edit-item', "mx2", "item-icon");
+// iconDiv.appendChild(link2);
+
+// const faEdit = document.createElement('i');
+// faEdit.classList.add("far", "fa-edit");
+// link2.appendChild(faEdit);
+
+
+// link3 =document.createElement('a');
+// link3.setAttribute('href', "#");
+// link3.classList.add('delete-item', "item-icon");
+// iconDiv.appendChild(link3);
+
+// const circleTime = document.createElement('i');
+// circleTime.classList.add("far", "fa-times-circle");
+// link3.appendChild(circleTime);
 
 
 divItem.appendChild(newDivEl);
@@ -70,7 +77,9 @@ divItem.appendChild(newDivEl);
 input.value="";
 
 
-newDivEl.querySelector('a.delete-item').addEventListener('click',removeTask)
+newDivEl.querySelector('a.delete-item').addEventListener('click',removeTask);
+
+// newDivEl.querySelector('a.complete-item').addEventListener('click', toggleClass);
 }
 
 form.addEventListener('click',addItem);
@@ -80,6 +89,26 @@ const removeTask = (e) => {
 
     e.target.parentNode.parentNode.parentNode.remove();
     console.log(e.target.parentNode.parentNode.parentNode.remove());
+}
+
+const toggleClass = (e) => {
+    console.log(e),
+    console.log('Its working');
+  
+// const h5 = document.querySelector('h5');
+// const completeItems = document.querySelector('a.complete-item');
+
+
+// h5.classList.toggle('completed');
+
+// completeItems.classList.toggle('visibility');
+
+
+// if (h5.classList.toggle('completed') && document.querySelector('a.complete-item').classList.toggle('visibility') ) {
+// h5.style.textDecoration = "line-through";
+// }
+    // e.target.parentNode.parentNode.style.textDecoration = "line-through";
+    // console.log(  e.target.parentNode.parentNode.parentNode);
 }
 
 
