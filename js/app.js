c650b0b12560d9905e93b36dca963f1463bef47a
Magdalena <magdalenaouirane@gmail.com>
2021-01-18 20:26:44 +0100

    add delete, update and  complete funcionality

--- js/app.js
+++ js/app.js
@@ -1,143 +1,159 @@
-const form = document.getElementById('itemForm');
+const form = document.getElementById("itemForm");
 
-const input = document.querySelector('#itemInput');
+const submitBtn = document.querySelector(".btn-green");
 
-const divItem = document.querySelector('.item-list');
+const input = document.querySelector("#itemInput");
 
-const clearBtn = document.getElementById('clear-list');
+const divItemList = document.querySelector(".item-list");
 
-const alertDiv = document.querySelector('.alert');
+const clearBtn = document.getElementById("clear-list");
+
+const alertDiv = document.querySelector(".alert");
 
 
+//edit option
 let editEl;
 let editFlag = false;
 let editId = "";
 
-
-
-
-
 const addItem = (e) => {
+    e.preventDefault();
+    console.log(e);
 
+    const newItem = input.value;
+    console.log(newItem);
 
-e.preventDefault();
-console.log(e);
+    const id = new Date().getTime().toString();
+    console.log(id);
 
-const newItem = input.value;
-console.log(newItem);
+    if (newItem !== "" && editFlag === false) {
+        const newDivEl = document.createElement("div");
+        console.log(newDivEl);
+        newDivEl.classList.add("item", "my-3");
 
-const id = new Date().getTime().toString();
-console.log(id);
+        //add id
+        const attr = document.createAttribute("data-id");
+        attr.value = id;
+        newDivEl.setAttributeNode(attr);
 
-
-if ( newItem !== "" && editFlag === false) {
-
-    const newDivEl = document.createElement('div');
-    console.log(newDivEl);
-    newDivEl.classList.add("item","my-3");
-
-    //add id
-    const attr = document.createAttribute('data-id');
-    attr.value = id;
-    newDivEl.setAttributeNode(attr);
-
-    newDivEl.innerHTML = `<h5 class="item-name text-capitalize">${newItem}</h5>
+        newDivEl.innerHTML = `<h5 class="item-name text-capitalize">${newItem}</h5>
     <div class="item-icons">
      <a href="#" class="complete-item mx-2 item-icon"><i class="far fa-check-circle"></i></a>
      <a href="#" class="edit-item mx-2 item-icon"><i class="far fa-edit"></i></a>
      <a href="#" class="delete-item item-icon"><i class="far fa-times-circle"></i></a>
     </div>`;
 
-    //append Child
-      divItem.appendChild(newDivEl);
+        const deleteBtn = newDivEl.querySelector("a.delete-item");
+        console.log(deleteBtn);
 
-    //add to localStorage
-    addToLocalStorage(id, newItem);
-    //set back to Default
-    setBackToDefault()
+        deleteBtn.addEventListener("click", removeTask);
 
+        const editBtn = newDivEl.querySelector("a.edit-item");
+        editBtn.addEventListener("click", editItem);
 
+        const completeBtn = newDivEl.querySelector("a.complete-item");
+        completeBtn.addEventListener("click", completedTask);
 
+        //append Child
+        divItemList.appendChild(newDivEl);
 
+        //add to localStorage
+        addToLocalStorage(id, newItem);
+        //set back to Default
+        setBackToDefault();
 
-    console.log("newItem added to the list")
-} else if (newItem !== "" && editFlag === true) {
-    console.log("editing");
-} else {
-   alertDiv.style.display = "block";
-  
-   setTimeout(function(){
-    alertDiv.style.display = "none"; 
-   },2000)
-}
+        console.log("newItem added to the list");
+    } else if (newItem !== "" && editFlag === true) {
+        editEl.innerHTML = newItem;
+        //edit local Storage
+        editLocalStorage(editId, newItem);
+        setBackToDefault();
+    } else {
+        alertDiv.style.display = "block";
+        //remove alert
+        setTimeout(function () {
+            alertDiv.style.display = "none";
+        }, 2000);
+    }
 
-//set back to default
-function setBackToDefault() {
-    console.log('set back to default');
-}
-
-// LOCAL STORAGE
-function addToLocalStorage(id, newItem) {
-    console.log('added to local storage');
-}
-
-
-
-
-
-input.value="";
+    //complete function
+    function completedTask(e) {
+        console.log("completed task");
 
 
-// newDivEl.querySelector('a.delete-item').addEventListener('click',removeTask);
-
-// newDivEl.querySelector('a.complete-item').addEventListener('click', toggleClass);
-}
-
-form.addEventListener('submit',addItem);
-
-
-const removeTask = (e) => {
-
-    e.target.parentNode.parentNode.parentNode.remove();
-    console.log(e.target.parentNode.parentNode.parentNode.remove());
-}
-
-const toggleClass = (e) => {
-    console.log(e),
-    console.log('Its working');
-  
-// const h5 = document.querySelector('h5');
-// const completeItems = document.querySelector('a.complete-item');
-
-
-// h5.classList.toggle('completed');
-
-// completeItems.classList.toggle('visibility');
-
-
-// if (h5.classList.toggle('completed') && document.querySelector('a.complete-item').classList.toggle('visibility') ) {
-// h5.style.textDecoration = "line-through";
-// }
-    // e.target.parentNode.parentNode.style.textDecoration = "line-through";
-    // console.log(  e.target.parentNode.parentNode.parentNode);
-}
+        const divs = divItemList.querySelectorAll(".item");
+        console.log("Number of divs in te list:", divs);
 
 
 
+        divs.forEach((div) => {
+            div.querySelector('.complete-item').classList.toggle("visibility");
 
 
-clearBtn.addEventListener('click', (e) => {
-     divItem.textContent = "";
-})
+            div.querySelector("h5").classList.toggle("completed");
 
+        });
 
+        setBackToDefault();
+    }
 
+    //edit function
+    function editItem(e) {
+        console.log("item is edited");
+        const element = e.currentTarget.parentElement.parentElement;
+        console.log('element', element)
+        //set edit
+        editEl = e.currentTarget.parentElement.previousElementSibling;
+        console.log('edited Element:', editEl)
+        //set form value
+        input.value = editEl.innerHTML;
+        editFlag = true;
+        editId = element.dataset.id;
+        submitBtn.textContent = "edit";
+    }
 
+    //delete function
 
+    function removeTask(e) {
+        console.log("task deleted");
+        // e.target.parentNode.parentNode.parentNode.remove();
+        // console.log(e.target.parentNode.parentNode.parentNode.remove());
 
+        const element = e.currentTarget.parentElement.parentElement;
+        const id = element.dataset.id;
+        divItemList.removeChild(element);
 
+        setBackToDefault();
+        //remove from the local storage
+        //removeFromLocalStorage(id);
+    }
 
+    //set back to default
+    function setBackToDefault() {
+        console.log("set back to default");
+        input.value = "";
+        editFlag = false;
+        editId = "";
+        submitBtn.textContent = "add item";
+    }
 
+    // LOCAL STORAGE
+    function addToLocalStorage(id, newItem) {
+        console.log("added to local storage");
+    }
 
+    function removeFromLocalStorage(id) {}
 
+    function editLocalStorage(id, newItem) {}
 
+};
+
+// EVENT LISTENERS
+
+form.addEventListener("submit", addItem);
+
+clearBtn.addEventListener("click", (e) => {
+    divItemList.textContent = "";
+    //setBackToDefault();
+    //localStorage.removeItem('divItem');
+});
